6a7a180222901d0d7cc9d8ed6011d7616680fd29
Pete Lewis <601236+lewispg228@users.noreply.github.com>
2023-12-22 16:26:30 -0700

    Ex1 and ex2 working

--- examples/Example_02_Buzz_Frequency/Example_02_Buzz_Frequency.ino
+++ examples/Example_02_Buzz_Frequency/Example_02_Buzz_Frequency.ino
@@ -1,9 +1,11 @@
 /******************************************************************************
   Example_01_Buzz
 
-  This example shows how to turn the buzzer on and off.
+  This example shows how to adjust the frequency of the buzzer.
+
+  It turns the buzzer on and off.
   Much like the classic "blink LED sketch" this will buzz
-  the buzzer once every second.
+  the buzzer once every second at a different frequency.
 
   By Pete Lewis @ SparkFun Electronics
   December 2023
@@ -25,7 +27,7 @@ QwiicBuzzer buzzer;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Qwiic Buzzer Example_01_Buzz");
+  Serial.println("Qwiic Buzzer Example_02_Buzz_Frequency");
   Wire.begin(); //Join I2C bus
 
   //check if button will acknowledge over I2C
@@ -37,11 +39,15 @@ void setup() {
 }
 
 void loop() {
-  buzzer.on();
-  
-  delay(1000);
+  buzzer.on(2730); // resonant frequency is 2.73KHz
+  delay(100);
   
   buzzer.off();
+  delay(1000);
+
+  buzzer.on(1000); // try out 1KHz for fun
+  delay(100);
   
+  buzzer.off();
   delay(1000);
 }
