b44569e18102f5141cf8e74ea9b5a4144916c424
Pete Lewis <601236+lewispg228@users.noreply.github.com>
2024-01-11 11:23:00 -0700

    ex3 duration working

---
+++ examples/Example_03_Buzz_Duration/Example_03_Buzz_Duration.ino
@@ -0,0 +1,50 @@
+/******************************************************************************
+  Example_03_Buzz_Duration
+
+  This example shows how to control the buzzer using frequency and duration.
+
+  It turns the buzzer on and off.
+  Much like the classic "blink LED sketch" this will buzz
+  the buzzer once every second at a different frequency and duration.
+
+  By Pete Lewis @ SparkFun Electronics
+  December 2023
+
+  Based on code originally written by Fischer Moseley @ SparkFun Electronics
+  Original Creation Date: June 28, 2019
+
+  This code is Lemonadeware; if you see me (or any other SparkFun employee) at the
+  local, and you've found our code helpful, please buy us a round!
+
+  Hardware Connections:
+  Connect QWIIC cable from Arduino to Qwiic Buzzer
+
+  Distributed as-is; no warranty is given.
+******************************************************************************/
+
+#include <SparkFun_Qwiic_Buzzer_Arduino_Library.h>
+QwiicBuzzer buzzer;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Qwiic Buzzer Example_03_Buzz_Duration");
+  Wire.begin(); //Join I2C bus
+
+  //check if buzzer will acknowledge over I2C
+  if (buzzer.begin() == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+  Serial.println("Buzzer acknowledged.");
+}
+
+void loop() {
+  buzzer.on(2730, 100); // frequency: 2.73KHz, duration: 100ms
+  delay(1000);
+
+  buzzer.on(1000, 500); // frequency: 1K, duration: 500ms
+  delay(1000);
+
+  // Note, we dont' have to call buzzer.off(), because it will automatically turn
+  // off after the duration of each tone is completed.
+}
